f944e1314123690539ab8e66263cfdc2c8c4fc39
UncleBrown <litq@liufeng-it11.funshion.com>
2019-06-25 19:14:53 +0800

    add config

---
+++ configure/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+
+#[mail]
+#发件人邮箱
+name = "Jeremy"
+send_user = "494181687@qq.com"
+password = "Jeremy@1314@."
+email_host = "smtp.qq.com"
+
+#收件人列表
+user_list = ['litq@fun.tv']
+
+#标题
+sub = "接口自动化测试报告"
+#正文
+content = "接口自动化测试结果:见附件"
